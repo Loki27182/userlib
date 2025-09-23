262b8a9d82f03f0622bf3a864011dddafe7e1e20
Loki27182 <jglee@umd.edu>
2025-09-22 22:43:35 -0400

    testing arduino dds controller

--- labscriptlib/SrMain/connection_table_simulated.py
+++ labscriptlib/SrMain/connection_table_simulated.py
@@ -1,22 +1,18 @@
-from labscript import start, stop, add_time_marker, AnalogOut, DigitalOut
+from labscript import start, stop, add_time_marker, AnalogOut, DigitalOut, ClockLine
 from labscript_devices.DummyPseudoclock.labscript_devices import DummyPseudoclock
 from labscript_devices.DummyIntermediateDevice import DummyIntermediateDevice
+from labscript_devices.lsduino import lsduino
+from labscript_devices.AD9910 import AD9910
 
-# Use a virtual, or 'dummy', device for the psuedoclock
 DummyPseudoclock(name='pseudoclock')
 
-# An output of this DummyPseudoclock is its 'clockline' attribute, which we use
-# to trigger children devices
-DummyIntermediateDevice(name='intermediate_device', parent_device=pseudoclock.clockline)
+lsduino(name='dds_controller', ndev=2, parent_device=pseudoclock.clockline, com_port='com6', 
+            baud_rate=115200, synchronous_first_line_repeat=True)
 
-# Create an AnalogOut child of the DummyIntermediateDevice
-AnalogOut(name='analog_out', parent_device=intermediate_device, connection='ao0')
-
-# Create a DigitalOut child of the DummyIntermediateDevice
-DigitalOut(
-    name='digital_out', parent_device=intermediate_device, connection='port0/line0'
-)
-
+AD9910(name='dds_0',   parent_device=dds_controller, connection='channel 0')
+AD9910(name='dds_1',   parent_device=dds_controller, connection='channel 1')
+#AD9910(name='dds_2',   parent_device=dds_controller, connection='channel 2')
+#AD9910(name='dds_3',   parent_device=dds_controller, connection='channel 3')
 
 if __name__ == '__main__':
     # Begin issuing labscript primitives
